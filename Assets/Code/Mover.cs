61649384eb921f8d2b8e67a76e0eb000d396bf67
Agon <agonthedev@gmail.com>
2023-11-14 06:42:09 -0800

    Click To Move
    Added code to allow click-to-move
    
    Initial code. Will be expanded for movement improvements and game interactions.

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -3,13 +3,9 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-
     public Transform Target;
 
     private NavMeshAgent nmAgent;
-
-    public float moveX = 0.0f;
-    public float moveY = 0.0f;
 
     void Start()
     {
@@ -18,28 +14,46 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
+        DetectMouseInput();
         DetectPlayerInput();
-
-        Target.position = new Vector3(transform.position.x + moveX, transform.position.y, transform.position.z + moveY);
-
-        nmAgent.SetDestination(Target.position);
-
     }
 
     private void DetectPlayerInput()
     {
-        moveX = 0.0f; 
-        moveY = 0.0f;
+        float moveX = 0.0f; 
+        float moveY = 0.0f;
 
         if ( Input.GetKey(KeyCode.W) ) { moveX += 3.0f; }
         if ( Input.GetKey(KeyCode.S) ) { moveX -= 3.0f; }
-        if ( Input.GetKey(KeyCode.D) ) { moveY += 3.0f; }
-        if ( Input.GetKey(KeyCode.A) ) { moveY -= 3.0f; }
+        if ( Input.GetKey(KeyCode.D) ) { moveY -= 3.0f; }
+        if ( Input.GetKey(KeyCode.A) ) { moveY += 3.0f; }
 
         if ( moveX != 0 && moveY != 0 )
         {
             moveX /= 2;
             moveY /= 2;
         }
+
+        if ( moveX != 0 || moveY != 0 )
+        {
+            Target.position = new Vector3(transform.position.x + moveX, transform.position.y, transform.position.z + moveY);
+            nmAgent.SetDestination(Target.position);
+        }
     }
+
+    private void DetectMouseInput()
+    {
+        if ( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
+            
+            if (hasHit)
+            {
+                Target.position = hit.point;
+                nmAgent.SetDestination(Target.position);
+            }
+        }
+    }
+
 }
